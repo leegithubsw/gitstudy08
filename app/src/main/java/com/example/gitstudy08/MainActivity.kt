629274f8a2af94a868d09cb1fc0081e8fe23d7a0
leesw <lswkor@naver.com>
2021-12-31 20:27:19 +0900

    first change
    간단한 tv, et, btn 삽입

--- app/src/main/java/com/example/gitstudy08/MainActivity.kt
+++ app/src/main/java/com/example/gitstudy08/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.gitstudy08
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    lateinit var tv1: TextView
+    lateinit var et1: EditText
+    lateinit var bt1: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findfiew()
+    }
+    fun findfiew() {
+        tv1 = findViewById(R.id.tv1)
+        et1 = findViewById(R.id.et1)
+        bt1 = findViewById(R.id.bt1)
+
+        bt1.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v) {
+            bt1 -> Toast.makeText(this, "${bt1.text} clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
